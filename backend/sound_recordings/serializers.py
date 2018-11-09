7e21c66084a60f783e630afa70070f369cb0770f
Josefina <josefinaestevez91@gmail.com>
2018-11-09 14:12:10 -0300

    matches list

--- backend/sound_recordings/serializers.py
+++ backend/sound_recordings/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
-from sound_recordings.models import InputReport, InputReportMatch
+from sound_recordings.models import SoundRecording, InputReport, InputReportMatch
+
+
+class SoundRecordingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SoundRecording
+        fields = ('artist', 'title', 'isrc', 'duration')
+
+
+class InputReportMatchSerializer(serializers.ModelSerializer):
+    sound_recording = SoundRecordingSerializer()
+
+    class Meta:
+        model = InputReportMatch
+        fields = ('pk', 'sound_recording')
 
 
 class InputReportSerializer(serializers.ModelSerializer):
+    matches = InputReportMatchSerializer(many=True)
 
     class Meta:
         model = InputReport
-        fields = ('pk', 'artist', 'title', 'isrc', 'duration', 'matched')
+        fields = ('pk', 'artist', 'title', 'isrc',
+                  'duration', 'matched', 'matches')
--- backend/sound_recordings/urls.py
+++ backend/sound_recordings/urls.py
@@ -4,7 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from sound_recordings import views
 
 urlpatterns = [
-	path('input_reports/', views.InputReportList.as_view()),
+    path('input_reports/', views.InputReportList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
